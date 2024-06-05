c62b202640fbedff4c7c2bffca91e9936690f790
Ja <ja.techup@gmail.com>
2024-06-05 13:32:23 +0700

    feat: added states

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,33 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [inputMessage, getInputMessage] = useState("");
+  const [displayMessage, getDisplayMessage] = useState("Greeting Message");
+
   return (
     <div className="App">
-      <div className="greeting-container">Greeting Message</div>
+      <div className="greeting-container">{displayMessage}</div>
       <div className="input-container">
         <label htmlFor="greeting-message">New Greeting Message</label>
-        <input id="greeting-message" type="text" />
+        <input
+          id="greeting-message"
+          type="text"
+          value={inputMessage}
+          onChange={(event) => {
+            getInputMessage(event.target.value);
+          }}
+        />
       </div>
 
       <div className="buttons">
-        <button>Update text</button>
+        <button
+          onClick={() => {
+            getDisplayMessage(inputMessage);
+          }}
+        >
+          Update text
+        </button>
       </div>
     </div>
   );
